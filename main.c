b790763affa61a0d9982d5d9b08c9ccba8117267
Dmitry Ulyanov <Dmitry.m.Ulyanov@megafon.ru>
2014-11-07 15:20:12 +0300

    инициализация

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: santa
+ *
+ * Created on 7 ноября 2014 г., 15:01
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
